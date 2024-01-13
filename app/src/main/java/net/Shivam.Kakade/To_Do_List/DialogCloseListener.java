3705d218b763807f03e04117430a9e6c430d4b84
ShivKakade <shivkakade143@gmail.com>
2024-01-13 11:11:40 +0530

    First commit

---
+++ app/src/main/java/net/Shivam.Kakade/To_Do_List/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package net.Shivam.Kakade.To_Do_List;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
